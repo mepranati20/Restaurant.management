c177ad97e3be82cf27dc395e1faadf865e90c18c
mepranati20 <t.pranatipatra123@gmail.com>
2025-12-22 09:39:00 +0000

    backend

--- frontend/assets/js/utils/dom.js
+++
@@ -1,9 +0,0 @@
-// Shortcut for selecting elements by ID
-export const $ = (id) => document.getElementById(id);
-
-// Converts an HTML string into a real DOM element
-export function createElement(html) {
-  const div = document.createElement("div");
-  div.innerHTML = html.trim();
-  return div.firstChild; // return the generated element
-}
--- frontend/assets/js/utils/loadComponent.js
+++
@@ -1,8 +0,0 @@
-// Load an HTML file into a target element
-export async function loadComponent(selector, url) {
-  const target = document.querySelector(selector);
-  if (!target) return;
-
-  const html = await fetch(url).then(res => res.text());
-  target.innerHTML = html;
-}
